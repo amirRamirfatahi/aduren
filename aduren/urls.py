57673724e5c0e1bc36e420d31501f309d70a17d4
AmirReza <amir.r.amirfatahi@gmail.com>
2018-11-25 19:43:23 +0330

    remove cached shit

--- aduren/urls.py
+++ aduren/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import index_view
+from core.views import index_view, project_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view)
+    path('', index_view),
+    path('project/<project_id>/', project_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from core.models import FirstPageProjects
+from core.models import FirstPageProjects, Project
 
 
 class IndexView(TemplateView):
@@ -12,4 +12,14 @@ class IndexView(TemplateView):
         return context
 
 
+class ProjectView(TemplateView):
+    template_name = 'ProjectItem.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectView, self).get_context_data(**kwargs)
+        context['project'] = Project.objects.get(id=context.get('project_id'))
+        return context
+
+
 index_view = IndexView.as_view()
+project_view = ProjectView.as_view()
