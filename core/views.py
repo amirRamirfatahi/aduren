29ece3ec19bbea73b3a563f5b0b7114f30eb088f
AmirReza <amir.r.amirfatahi@gmail.com>
2018-11-25 19:21:55 +0330

    add index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from core.models import FirstPageProjects
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['first_page_projects'] = FirstPageProjects.load().projects.all()
+        return context
+
+
+index_view = IndexView.as_view()
